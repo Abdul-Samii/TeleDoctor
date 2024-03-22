1fe980bfde2afd410c8ad8709fc006d22206e2c3
Abdul Sami <ksamk100474@gmail.com>
2024-03-22 16:32:45 +0500

    Add project files.

---
+++ TeleDoctor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeleDoctor.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
